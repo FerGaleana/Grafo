1da3307addd60c61fed43d3e2bd962f272e9a5df
karlaramireze <39529763+karlaramireze@users.noreply.github.com>
2018-07-10 12:05:40 -0500

    Update funcion destroy

--- Graph.c
+++ Graph.c
@@ -36,8 +36,30 @@ Graph graph_create(CMP comparator,Clone clone, MyFree mfree){
 }
 
 void graph_destroy(Graph g){
-	for(int i=0; i<g->V; i++)
-		free(g->arr[i]);
-	free(g);
-
+	
+	Node aux, next;
+	int i;
+	
+	for(i=0; i<g->V; i++){ //Va a ir recorriendo hasta el total de vertices
+		
+		if(g->arr[i] != NULL){ //Mientras que se sigan encontrando vertices
+			aux= g->arr[i]; //Mi auxiliar toma el valor de ese vertice
+			
+			while (aux!= NULL){ //Si sigue tomando valores
+				next= aux->next; //Se irA recorriendo
+				
+				if(g->myFree != NULL){ //Funcion myFree
+					g->myFree(aux->id); //Se libera el id de mi auxiliar
+					g->myFree(aux->data); //Se libera el dato de mi auxiliar
+				}
+				
+				free(aux); //Se libera mi auxiliar
+				aux=next; //Mi auxiliar se recorre
+			}//fin while
+			
+			free(g->arr[i]); //Se libera la memoria de mi arreglo[i]
+		}//fin if
+	}//fin for
+	free(g->arr); //Se libera la memoria de mi arreglo por completo
+	free(g); //Se libera la memoria de mi grafo
 }
