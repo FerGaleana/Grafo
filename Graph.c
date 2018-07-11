713053eb26797aa5f407f75f2a8f1ae6dae0df75
FerGaleana <39529955+FerGaleana@users.noreply.github.com>
2018-07-11 00:05:08 -0500

    Update Graph.c

--- Graph.c
+++ Graph.c
@@ -36,7 +36,7 @@ Graph graph_create(CMP comparator,Clone clone, MyFree mfree){
 }
 
 void graph_destroy(Graph g){
-	unsigned int v= g->n_v;
+	unsigned int v=g->n_v;
 	for(int i=0; i<v; i++){
 		if(g->arr[i]->next!=NULL)
 			list_destroy(g->arr[i]->next);
@@ -47,9 +47,10 @@ void graph_destroy(Graph g){
 
 }
 
-//Agrega un apuntador al arreglo vértices e inicializa los datos
+//Funciones para arreglo dinámico
+//Crea el primer apuntador dentro del arreglo vértices (se manda a llamar si es el primer vértice) e inicializa los datos
 GNode* add_array(Type data, unsigned int id){
-	GNode* newVertex= (GNode*)malloc(sizeof(struct GstrNode));
+	GNode* newVertex=(GNode*)malloc(sizeof(struct GstrNode));
 	if(newVertex!=NULL){
 		newVertex->data=data;
 		newVertex->id=id;
@@ -62,46 +63,46 @@ GNode* add_array(Type data, unsigned int id){
 }
 
 Bool graph_addVertex(Graph g, Type data){
-	GNode **arreglo= g->arr;
-	unsigned int i= g->n_v;
-	Bool found= false;
-	if(i==0 && arreglo==NULL){
-		arreglo= (GNode**)malloc(sizeof(GNode*)); //Crea un nuevo arreglo de apuntadores
-		if(arreglo == NULL)
+	GNode **arreglo=g->arr;
+	unsigned int i=g->n_v;
+	Bool found=false;
+	if(i==0&&arreglo==NULL){
+		arreglo=(GNode**)malloc(sizeof(GNode*)); //Crea un nuevo arreglo de apuntadores
+		if(arreglo==NULL){
 			return false;
+		}
 		else
 		g->arr=arreglo;
 	}
 	else{
 		int j=0;
 		//Busca que no exista el dato
-		while(found==false && j<i){
-			if(g->cmpFunction(data,(arreglo[j]->data)) == 0){
+		while(found==false&&j<i){
+			if(g->cmpFunction(data,(arreglo[j]->data))==0){
 				found=true;
 				return false;
 			}
 			j++;
 		}
 	}
-	if(found==false && arreglo!=NULL){
+	if(found==false&&arreglo!=NULL){
 		if(i>0){
-			GNode** arregloTemp= (GNode**)realloc(arreglo,sizeof(GNode*)*(g->n_v+1));
-				if(arregloTemp != NULL)
-					arreglo= arregloTemp;
+			GNode** arregloTemp=(GNode**)realloc(arreglo,sizeof(GNode*)*(g->n_v+1));
+			if(arregloTemp!=NULL)
+				arreglo=arregloTemp;
 		}
-		
 		arreglo[i]=add_array(g->myClone(data),i+1);
-		if(arreglo[i] == NULL)
-			return false;
-		
+		if(arreglo[i]==NULL){
+					return false;
+		}
 		g->arr=arreglo;
 		g->n_v++;
 	}
 	else
 		return false;
-
 	return true;
 }
+
 //Buscar vértice
 GNode* searchVertex(Graph g,Type source){
 	Bool found=false;
@@ -141,6 +142,25 @@ Bool graph_hasEdge(Graph g,Type source,Type skin){
 	}
 	return false;
 }
+Bool graph_addEdge(Graph g, Type source, Type skin){
+	GNode *source_node=searchVertex(g,source);
+	GNode *skin_node=searchVertex(g,skin);
+	//List l;
+	//unsigned int i=g->A;
+	if(source_node!=NULL&&skin_node!=NULL&&source_node!=skin_node){
+		if(source_node->next==NULL){
+			source_node->next=list_create();
+		}
+		if(graph_hasEdge(g,source,skin)==false){
+			list_add(source_node->next,(struct strNode*)skin_node);
+			g->A++;
+			return true;
+		}
+	}
+		return false;
+
+}
+
 unsigned int graph_vertexCount(Graph g){
 	return g->n_v;
 }
@@ -156,4 +176,28 @@ unsigned int graph_outDegree(Graph g,Type source){
 		size=list_size(current->next);
 	}
 	return size;
+}
+
+
+Bool graph_print(Graph g, Print p){
+	GNode **edge=g->arr;
+	GNode *temp;
+	int i;
+	if(g!=NULL){
+		printf("Vértices: ");
+		for(i=0;i<g->n_v;i++){
+			printf("\nID: %u  ",edge[i]->id);
+			p(edge[i]->data);
+			if(edge[i]->next!=NULL){
+				printf("\tAristas: ");
+			for(int j=0;j<list_size(edge[i]->next);j++){
+				temp=(GNode*)list_get(edge[i]->next,j);
+				p(temp->data);
+			}
+			}
+
+		}
+		return true;
+	}
+	return false;
 }
